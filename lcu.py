548ca6f0db18fefd8a7cbccd9c59e585bedd166f
Wei Wu <45323446+U-rara@users.noreply.github.com>
2022-05-31 16:16:50 +0800

    Create lcu.py
    支持西部魔影(2022)通行证

---
+++ lcu.py
@@ -0,0 +1,19 @@
+from lcu_driver import Connector
+
+connector = Connector()
+
+@connector.ready
+async def connect(connection):
+    print('LCU API is ready to be used.')
+    summoner = await connection.request('post', '/lol-loot/v1/recipes/MATERIAL_536_FORGE_2/craft',json=['MATERIAL_536'])
+    json = await summoner.json()
+    print(json)
+    f2 = open('output.json', 'w')
+    f2.write(str(json))
+    f2.close()
+
+@connector.close
+async def disconnect(_):
+    print('The client have been closed!')
+
+connector.start()
